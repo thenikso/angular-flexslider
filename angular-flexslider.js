309f44e36ceab9e1a8c64dd3289247e233b51219
Benoit Tremblay <trembl.ben@gmail.com>
2014-09-07 11:59:29 -0400

    Support slide as well as flex-slide attribute

--- angular-flexslider.js
+++ angular-flexslider.js
@@ -12,7 +12,7 @@
           return function($scope, $element) {
             var addSlide, collectionString, flexsliderDiv, getTrackFromItem, indexString, match, removeSlide, slidesItems, trackBy;
 
-            match = attr.slide.match(/^\s*(.+)\s+in\s+(.*?)(?:\s+track\s+by\s+(.+?))?\s*$/);
+            match = (attr.slide || attr.flexSlide).match(/^\s*(.+)\s+in\s+(.*?)(?:\s+track\s+by\s+(.+?))?\s*$/);
             indexString = match[1];
             collectionString = match[2];
             trackBy = angular.isDefined(match[3]) ? $parse(match[3]) : $parse("" + indexString);
